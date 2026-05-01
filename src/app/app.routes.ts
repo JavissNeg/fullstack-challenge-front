510abac730c55715dc9ae41fe17eb2095d16f651
Javiss <javiss@2806-105e-0015-cd6a-0000-0000-0000-0002.ipv6.infinitum.net.mx>
2026-05-01 15:00:02 -0600

    chore(app): finalize app configuration, routing and dependencies setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { FlightsDashboardComponent } from './modules/flights/pages/flights-dashboard/flights-dashboard';
+import { StackDashboardComponent } from './modules/stack/pages/stack-dashboard/stack-dashboard';
+import { MainLayout } from './shared/layout/main-layout/main-layout';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      {
+        path: 'flights',
+        component: FlightsDashboardComponent,
+        data: { title: 'Flight Metrics' }
+      },
+      {
+        path: 'stack',
+        component: StackDashboardComponent,
+        data: { title: 'Stack Metrics' }
+      },
+      {
+        path: '',
+        redirectTo: 'flights',
+        pathMatch: 'full',
+      },
+    ],
+  },
+];
